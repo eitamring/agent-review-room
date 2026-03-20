3873d5f6ab4392015b2ada6f83d975b2595f341f
eitam <eitamos10@gmail.com>
2026-03-20 09:02:06 -0700

    Remove auto-open DevTools in dev mode

--- src/main/app.ts
+++ src/main/app.ts
@@ -46,7 +46,6 @@ export function createAppWindow(): BrowserWindow {
 
   if (isDev) {
     mainWindow.loadURL('http://localhost:5173');
-    mainWindow.webContents.openDevTools({ mode: 'bottom' });
   } else {
     mainWindow.loadFile(path.join(__dirname, '../renderer/index.html'));
   }
